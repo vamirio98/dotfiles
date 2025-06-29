3c434eef8541a1ddfaa56878bba878e6622d7f49
vamirio <vamirio@qq.com>
2025-06-29 21:17:57 +0800

    fix(syntax): fix syntax error

--- wsl/boot.sh
+++ wsl/boot.sh
@@ -29,7 +29,7 @@ mount_point=("/home")
 
 # Execute mount
 for vhd in "${vhd_file_path[@]}"; do
-	/mnt/c/Users/$username/AppData/Local/Microsoft/WindowsApps/wsl.exe -d $distribution --mount --vhd $vhd --bare
+	/mnt/c/Users/$username/AppData/Local/Microsoft/WindowsApps/wsl.exe -d "$distribution" --mount --vhd "$vhd" --bare
 done
 
 # Initialize elapsed time counter
@@ -50,21 +50,21 @@ while true; do
 	fi
 
     if [ $elapsed -ge $timeout ]; then
-        echo "Timed out waiting for "$drive" to be ready."
+        echo "Timed out waiting for $drive to be ready."
         exit 1
     fi
 
-    echo "Waiting for "$drive" to be ready..."
+    echo "Waiting for $drive to be ready..."
     sleep $interval
     elapsed=$((elapsed + interval))
 done
 
 # Mount the target drive
 for (( i = 0; i < ${#target_drive[@]}; i++ )); do
-	mount ${target_drive[${i}]} ${mount_point[${i}]}
+	mount "${target_drive[${i}]}" "${mount_point[${i}]}"
 done
 
 # Check the mounted target drive
 for drive in "${target_drive[@]}"; do
-	mount -l | grep ${drive}
+	mount -l | grep "${drive}"
 done
