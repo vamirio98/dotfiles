ac68b8688b40137cc456d77b2a2c4f544bb26121
vamirio <vamirio@qq.com>
2026-02-19 21:17:36 +0800

    update prompt.ps1

--- pwsh/prompt.ps1
+++ pwsh/prompt.ps1
@@ -4,29 +4,6 @@ function prompt
 				[Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole(
 			[Security.Principal.WindowsBuiltinRole]::Administrator)
 
-	# {{{ Calculate execution time of last cmd and convert to milliseconds,
-	# seconds or minutes
-	function CalElapsedTime()
-	{
-		$lastCommand = Get-History -Count 1
-		if ($lastCommand) {
-			$runTime = ($lastCommand.EndExecutionTime -
-					$lastCommand.StartExecutionTime).TotalSeconds
-		}
-
-		if ($runTime -ge 60) {
-			$ts = [timespan]::fromseconds($runTime)
-			$min, $sec = ($ts.ToString("mm\:ss")).Split(":")
-			$elapsedTime = -join ($min, " min ", $sec, " sec")
-		}
-		else {
-			$elapsedTime = [math]::Round($runTime, 2)
-			$elapsedTime = -join (($elapsedTime.ToString()), " sec")
-		}
-		return $elapsedTime
-	}
-	# }}}
-
 	# {{{ git info
 	function GetGitBranch() {
 		$gitBranch = git branch | Out-String
@@ -42,13 +19,16 @@ function prompt
 	# }}}
 
 	# Decorate the CMD Prompt
+	$colorNone = "`e[0m"
+	$colorBgCyan = "`e[46m"
+	$colorFgGreen = "`e[32m"
+	$colorFgBlue = "`e[34m"
 	$gitBranch = GetGitBranch
-	Write-Host ( $( if ([string]::IsNullOrEmpty($gitBranch)) { "" } else { "  $gitBranch ☰ " } ) ) -BackgroundColor Cyan -NoNewlin
-	Write-Host ""  # otherwise the 'Cyan' color above will append to the following line after execute any command
-	Write-Host "$env:username@$(hostname) " -ForegroundColor Green -NoNewline
-	Write-Host "$pwd " -ForegroundColor Blue -NoNewline
-	$elapsedTime = CalElapsedTime
-	Write-Host "[$elapsedTime] " -ForegroundColor Yellow
-	Write-Host ($(if ($isAdmin) { '#' } else { '$' })) -NoNewline
-	return " "
+	$gitBranch = $( if ([string]::IsNullOrEmpty($gitBranch)) { "" } else { "  $gitBranch ☰ " } )
+	$gitBranch = $( if ([string]::IsNullOrEmpty($gitBranch)) { "`r`n" } else { "${gitBranch}`r`n" } )
+	$res = "${colorBgCyan}${gitBranch}${colorNone}"
+	$res = "${res}${colorFgGreen}${env:username}@$(hostname)${colorNone}"
+	$res = "${res} ${colorFgBlue}${pwd}${colorNone}"
+	$res = "${res}`r`n$( if ($isAdmin) { '#' } else { '$' } ) "
+	return $res
 }
