478d76a92527344ace7909650602b04520fd29fa
vamirio <vamirio@qq.com>
2026-02-19 22:04:55 +0800

    remove git branch prompt in pwsh prompt

--- pwsh/prompt.ps1
+++ pwsh/prompt.ps1
@@ -19,15 +19,18 @@ function prompt
 	# }}}
 
 	# Decorate the CMD Prompt
+	# see: https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
 	$colorNone = "`e[0m"
 	$colorBgCyan = "`e[46m"
 	$colorFgGreen = "`e[32m"
 	$colorFgBlue = "`e[34m"
 	$gitBranch = GetGitBranch
-	$gitBranch = $( if ([string]::IsNullOrEmpty($gitBranch)) { "" } else { "  $gitBranch ☰ " } )
-	$gitBranch = $( if ([string]::IsNullOrEmpty($gitBranch)) { "`r`n" } else { "${gitBranch}`r`n" } )
-	$res = "${colorBgCyan}${gitBranch}${colorNone}"
-	$res = "${res}${colorFgGreen}${env:username}@$(hostname)${colorNone}"
+	# pwsh has bug with background color when scrolling,
+	# see: https://github.com/PowerShell/PowerShell/issues/15130
+	# $gitBranch = $( if ([string]::IsNullOrEmpty($gitBranch)) { "" } else { "[  $gitBranch  ]" } )
+	# $gitBranch = $( if ([string]::IsNullOrEmpty($gitBranch)) { "`r`n" } else { "${gitBranch}`r`n" } )
+	# $res = "${colorBgCyan}${gitBranch}${colorNone}"
+	$res = "${res}`r`n${colorFgGreen}${env:username}@$(hostname)${colorNone}"
 	$res = "${res} ${colorFgBlue}${pwd}${colorNone}"
 	$res = "${res}`r`n$( if ($isAdmin) { '#' } else { '$' } ) "
 	return $res
